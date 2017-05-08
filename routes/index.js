dcf467086e532dc5bca8393c596dad28343c0c00
Cal McLean <calmcl1@aol.com>
2017-05-08 18:11:51 +0100

    added theoretical boilerplate for groups api

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Note: this is boilerplate and has NOT been implemented yet */
+router.get('/groups', function(req,res,next){
+  res.render('groups_list')
+})
+
+/* Note: this is boilerplate and has NOT been implemented yet */
+router.get('/groups/{id}', function(req, res, next){
+  res.render('groups_list_contacts', {id: id})
+})
+
+/* Note: this is boilerplate and has NOT been implemented yet */
+router.post('/groups', function(req, res, next){
+  res.render('groups_create', {data: req.params}) // Is this right?
+})
+
+/* Note: this is boilerplate and has NOT been implemented yet */
+router.put('/groups/{id}', function(req, res, next){
+  res.render('groups_update', {group_id: id, data: req.params}) // Is this right?
+})
+
+/* Note: this is boilerplate and has NOT been implemented yet */
+router.delete('/groups/{id}', function(req, res, next){
+  res.render('groups_delete', {group_id: id})
+})
+
 module.exports = router;
