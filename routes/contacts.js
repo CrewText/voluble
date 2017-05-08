07afddf7603a0a002ef9828203bf3391b94d2cee
Cal McLean <calmcl1@aol.com>
2017-05-08 18:18:45 +0100

    updated groups routing syntax

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,27 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.get('/', function(req,res,next){
+router.get('/groups', function(req,res,next){
   res.render('groups_list')
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.get('/{id}', function(req, res, next){
+router.get('/groups/{id}', function(req, res, next){
   res.render('groups_list_contacts', {id: id})
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.post('/', function(req, res, next){
+router.post('/groups', function(req, res, next){
   res.render('groups_create', {data: req.params}) // Is this right?
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.put('/{id}', function(req, res, next){
+router.put('/groups/{id}', function(req, res, next){
   res.render('groups_update', {group_id: id, data: req.params}) // Is this right?
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.delete('/{id}', function(req, res, next){
+router.delete('/groups/{id}', function(req, res, next){
   res.render('groups_delete', {group_id: id})
 })
 
