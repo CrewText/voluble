877843a53ded3e038445dd1c6e20dbeeb49f29ae
Cal McLean <calmcl1@aol.com>
2017-08-24 09:35:13 +0100

    Add new contact to DB from POST request

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,19 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var dbClient = require('mariasql');
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.get('/', function(req,res,next){
-  res.render('contacts_list')
+  //res.render('contacts_list')
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.get('/{id}', function(req, res, next){
-  res.render('contact_info', {contact_id: id})
+  //res.render('contact_info', {contact_id: id})
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.post('/', function(req, res, next){
-  res.render('contacts_create', {data: req.params}) // Is this right?
+  //res.render('contacts_create', {data: req.params}) // Is this right?
+  
+  // Add a random contact
+  let client = new dbClient({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+  });
+
+  let prep = client.prepare("INSERT INTO `voluble`.`contacts` (`first_name`, `surname`, `email_address`, `default_servicechain`) VALUES (?, ?, ?, '1')")
+  client.query(prep([req.body.first_name, req.body.surname, req.body.email_address]), function(err, rows){
+    if (err)
+      throw err;
+
+    console.dir(rows);
+  })
+
+  res.send(`Inserted ${req.body.first_name} ${req.body.surname}!`);
+
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
