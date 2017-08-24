3f104dd669d200e4a0a3089e2747e816620b1de6
Cal McLean <calmcl1@aol.com>
2017-08-24 14:00:59 +0100

    get contact info on /contact/:id

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,16 +12,26 @@ var client = new dbClient({
 router.get('/', function(req,res,next){
   //res.render('contacts_list')
   let prep = client.prepare("SELECT first_name, surname, email_address FROM `voluble`.`contacts` ORDER BY id ASC")
-  client.query(prep(), function(err, rows){
+  client.query(prep(), null, {useArray: true}, function(err, rows){
     if (err){throw err}
     console.dir(rows)
-    console.log("things!");
+    res.send()
   })
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.get('/{id}', function(req, res, next){
+router.get('/:id', function(req, res, next){
   //res.render('contact_info', {contact_id: id})
+  console.log("Got param: " + req.params.id);
+
+  let query = client.prepare();
+  client.query("SELECT * FROM voluble.contacts WHERE id = ?", [parseInt(req.params.id)], {useArray: true}, function (err, rows){
+    if (err){throw err};
+
+    console.dir(rows);
+    res.send();
+  })
+
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
