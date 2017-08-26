235c88a289c446fcfc4592c8169198c3a6dcea87
Cal McLean <calmcl1@aol.com>
2017-08-26 10:13:41 +0100

    Formatting

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,45 +8,53 @@ var client = new dbClient({
   password: ''
 });
 
-/* Note: this is boilerplate and has NOT been implemented yet */
-router.get('/', function(req,res,next){
-  //res.render('contacts_list')
+
+router.get('/', function (req, res, next) {
+
   let prep = client.prepare("SELECT first_name, surname, email_address FROM `voluble`.`contacts` ORDER BY id ASC")
-    let q = client.query(prep())
-    let contacts = []
+  let q = client.query(prep())
+  let contacts = []
 
-    q.on('result', function(result){
-      result.on('data', function (row){
-          contacts.push(row)
-      })
-    }).on('end', function(){
-      res.json(contacts)
+  q.on('result', function (result) {
+    result.on('data', function (row) {
+      contacts.push(row)
     })
-
-  
+  }).on('end', function () {
+    res.json(contacts).status(200)
+  })
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.get('/:id', function(req, res, next){
-  //res.render('contact_info', {contact_id: id})
-  console.log("Got param: " + req.params.id);
+router.get('/:id', function (req, res, next) {
+  /* TODO: add validity checking - make sure the contact exists! */
+  /* TODO: add validity checking - make sure the param is a number */
+
+  let contact_id = null
+  try {
+    contact_id = parseInt(req.params.id)
+  } catch (e) {
+    res.status(500).send("Supplied contact ID is not an integer!")
+    return
+  }
 
   let query = client.prepare();
-  client.query("SELECT * FROM voluble.contacts WHERE id = ?", [parseInt(req.params.id)], {useArray: true}, function (err, rows){
-    if (err){throw err};
+  client.query("SELECT * FROM voluble.contacts WHERE id = ?", [parseInt(req.params.id)], { useArray: true }, function (err, rows) {
+    if (err) { throw err };
     res.json(rows)
   })
 
 })
 
+
+
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.post('/', function(req, res, next){
+router.post('/', function (req, res, next) {
   //res.render('contacts_create', {data: req.params}) // Is this right?
-  
+
   // Add a random contact
 
   let prep = client.prepare("INSERT INTO `voluble`.`contacts` (`first_name`, `surname`, `email_address`, `default_servicechain`) VALUES (?, ?, ?, '1')")
-  client.query(prep([req.body.first_name, req.body.surname, req.body.email_address]), function(err, rows){
+  client.query(prep([req.body.first_name, req.body.surname, req.body.email_address]), function (err, rows) {
     if (err)
       throw err;
 
@@ -58,13 +66,13 @@ router.post('/', function(req, res, next){
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.put('/{id}', function(req, res, next){
-  res.render('contacts_update', {group_id: id, data: req.params}) // Is this right?
+router.put('/{id}', function (req, res, next) {
+  res.render('contacts_update', { group_id: id, data: req.params }) // Is this right?
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.delete('/{id}', function(req, res, next){
-  res.render('contacts_delete', {group_id: id})
+router.delete('/{id}', function (req, res, next) {
+  res.render('contacts_delete', { group_id: id })
 })
 
 module.exports = router;
