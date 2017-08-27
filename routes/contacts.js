098464d9a5ad55965bf34db2478c3285ea92c754
Cal McLean <calmcl1@aol.com>
2017-08-27 10:36:18 +0100

    Add validity checking

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,12 +11,13 @@ var client = new dbClient({
 
 router.get('/', function (req, res, next) {
 
-  let prep = client.prepare("SELECT first_name, surname, email_address FROM `voluble`.`contacts` ORDER BY id ASC")
+  let prep = client.prepare("SELECT * FROM `voluble`.`contacts` ORDER BY id ASC")
   let q = client.query(prep())
   let contacts = []
 
   q.on('result', function (result) {
     result.on('data', function (row) {
+      row.url = req.protocol + '://' + req.get('host') + "/contacts/" + row.id
       contacts.push(row)
     })
   }).on('end', function () {
@@ -24,28 +25,32 @@ router.get('/', function (req, res, next) {
   })
 })
 
-/* Note: this is boilerplate and has NOT been implemented yet */
-router.get('/:id', function (req, res, next) {
-  /* TODO: add validity checking - make sure the contact exists! */
-  /* TODO: add validity checking - make sure the param is a number */
+router.get('/:contact_id', function (req, res, next) {
+  let contact_id = parseInt(req.params.contact_id)
 
-  let contact_id = null
-  try {
-    contact_id = parseInt(req.params.id)
-  } catch (e) {
-    res.status(500).send("Supplied contact ID is not an integer!")
+  // Check that the supplied contact ID is a valid int
+  if (!contact_id){
+    let error = {error: "Supplied contact ID is not an integer!"}
+    res.status(200).json(error)
     return
   }
-
+  
+  // Make the DB request for contact with ID `contact_id`
   let query = client.prepare();
-  client.query("SELECT * FROM voluble.contacts WHERE id = ?", [parseInt(req.params.id)], { useArray: true }, function (err, rows) {
+  client.query("SELECT * FROM voluble.contacts WHERE id = ?", [contact_id], { useArray: true }, function (err, rows) {
     if (err) { throw err };
-    res.json(rows)
+    
+    // Check that the contact actually exists
+    if (!rows.length){
+      let error = {error: "Contact with this ID does not exist."}
+      res.status(200).json(error)
+    } else {
+      // And if it does, return it!
+      res.json(rows)
+    }
   })
 
 })
-
-
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.post('/', function (req, res, next) {
