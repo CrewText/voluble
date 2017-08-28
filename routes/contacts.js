3f916ff9b858fafb92b0c7c648fd7ad8e1e508cc
Cal McLean <calmcl1@aol.com>
2017-08-28 08:13:10 +0100

    Made DELETE operation use promises

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var dbClient = require('mariasql');
+var Q = require('q');
 
 var client = new dbClient({
   host: 'localhost',
   user: 'root',
   password: ''
 });
+
+function verifyIdIsInteger(id) {
+  let deferred = Q.defer()
+
+  let parsed_id = parseInt(id)
+  if (!parsed_id) {
+    deferred.reject(new Error("Supplied ID is not an integer"))
+  }
+  else {
+    console.log("ID is valid: " + parsed_id)
+    deferred.resolve(parsed_id)
+  }
+
+  return deferred.promise
+}
+
+function deleteContactFromDB(id) {
+  let deferred = Q.defer()
+  client.query("DELETE FROM voluble.contacts WHERE id = ?", [id], true, function (err, rows) {
+    if (err) { deferred.reject(err) }
+    else {
+      console.log("Successfully deleted contact " + id)
+      deferred.resolve()
+    }
+  })
+
+  return deferred.promise
+}
 
 
 router.get('/', function (req, res, next) {
@@ -29,19 +58,19 @@ router.get('/:contact_id', function (req, res, next) {
   let contact_id = parseInt(req.params.contact_id)
 
   // Check that the supplied contact ID is a valid int
-  if (!contact_id){
-    let error = {error: "Supplied contact ID is not an integer!"}
+  if (!contact_id) {
+    let error = { error: "Supplied contact ID is not an integer!" }
     res.status(200).json(error)
     return
   }
-  
+
   // Make the DB request for contact with ID `contact_id`
   client.query("SELECT * FROM voluble.contacts WHERE id = ?", [contact_id], { useArray: true }, function (err, rows) {
     if (err) { throw err };
-    
+
     // Check that the contact actually exists
-    if (!rows.length){
-      let error = {error: "Contact with this ID does not exist."}
+    if (!rows.length) {
+      let error = { error: "Contact with this ID does not exist." }
       res.status(200).json(error)
     } else {
       // And if it does, return it!
@@ -76,21 +105,18 @@ router.put('/{id}', function (req, res, next) {
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.delete('/:contact_id', function (req, res, next) {
-  let contact_id = parseInt(req.params.contact_id)
-  // Check that the supplied contact ID is a valid int
-  if (!contact_id){
-    let error = {error: "Supplied contact ID is not an integer!"}
-    res.status(200).json(error)
-    return
-  }
-
-  client.query("DELETE FROM voluble.contacts WHERE id = ?", [contact_id], true, function(err,rows){
-    if (err){throw err}
-    else{
-      res.status(200).end()
-    }
-  })
-  
+  verifyIdIsInteger(req.params.contact_id)
+    .then(function (contact_id) {
+      return deleteContactFromDB(contact_id)
+    })
+    .then(function () {
+      res.send("Successfully deleted contact " + req.params.contact_id)
+    })
+    .catch(function(error){
+      console.log(error.message)
+      res.status(500).end()
+    })
+    .done()
 })
 
 module.exports = router;
