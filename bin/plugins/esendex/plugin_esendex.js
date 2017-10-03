40687d90349b1e83e04858bc8697c66ff2278ef0
Cal McLean <calmcl1@aol.com>
2017-10-03 11:08:57 +0100

    plugin_esendex.js now uses Promises

--- bin/plugins/esendex/plugin_esendex.js
+++ bin/plugins/esendex/plugin_esendex.js
@@ -1,49 +1,55 @@
 // @ts-check
 var voluble_plugin_base = require('../plugin_base.js')
-var esendex = require('esendex') // Add authentication info here
+var Q = require('Q')
+var esendex = require('esendex')({
+  username: "me@myemail.com",
+  password: "this_pass_is_secure"
+}) // Add authentication info here
 
 var EsendexPlugin = {
-    name: "Esendex",
-    description: "A plugin allowing Esendex to be used a an SMS Service Provider",
+  name: "Esendex",
+  description: "A plugin allowing Esendex to be used a an SMS Service Provider",
 
-    username: null,
-    password: null,
-    account_ref: null,
+  username: null,
+  password: null,
+  account_ref: null
+}
 
-    send_message: function (message) {
-        // This must be defined by *every* function. It will be called by the voluble when a message needs to be sent.
-        console.log("Sending the message: " + message).
-        esendex_send_message(message.phone_number, message.message_text)
-        return voluble_plugin_base.message_states.MESSAGE_SENT
+EsendexPlugin.send_message = function (message) {
+  let deferred = Q.defer()
+  // This must be defined by *every* plugin. It will be called by the voluble when a message needs to be sent.
+  console.log("Sending the message: " + message)
+  return this.esendex_send_message(message.phone_number, message.message_text)
+
+}
+
+EsendexPlugin.esendex_send_message = function (phone_number, message_text) {
+  let deferred = Q.defer()
+  let message = {
+    accountreference: this.account_ref,
+    message: [{
+      to: phone_number,
+      body: message_text
+    }]
+  }
+
+  esendex.messages.send(message, function (err, response) {
+    if (err) {
+      console.log('Error sending message: ', err)
+      console.log(response);
+      deferred.reject(voluble_plugin_base.message.message_states.MESSAGE_FAILED)
     }
-
-    esendex_send_message: function(phone_number, message_text){
-      let message = {
-        accountreference: this.account_ref,
-        message:[{
-          to: phone_number,
-          body: message_text
-        }]
-      }
-
-      esendex.messages.send(message, function (err, response){
-        if (err){
-          console.log('Error sending message: ', err)
-          console.log(response);
-          return voluble_plugin_base.message_states.MESSAGE_FAILED
-        }
-      })
-    }
+  })
 }
 
 var createEsendexPlugin = function (username, password, account_ref) {
-    let es_plug = Object.assign(Object.create(voluble_plugin_base.voluble_plugin), EsendexPlugin)
+  let es_plug = Object.assign(Object.create(voluble_plugin_base.voluble_plugin), EsendexPlugin)
 
-    es_plug.username = username
-    es_plug.password = password
-    es_plug.account_ref = account_ref
+  es_plug.username = username
+  es_plug.password = password
+  es_plug.account_ref = account_ref
 
-    return es_plug
+  return es_plug
 }
 
-module.exports = {createEsendexPlugin};
+module.exports = { createEsendexPlugin };
