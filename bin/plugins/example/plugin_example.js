a93f021a7db1bfc3511563f3047e1b93b137dfdc
Cal McLean <calmcl1@aol.com>
2017-10-03 16:53:17 +0100

    Moved user-definable settings to manifest.json

--- bin/plugins/example/plugin_example.js
+++ bin/plugins/example/plugin_example.js
@@ -1,16 +1,31 @@
-// @ts-check
 var voluble_plugin_base = require('../plugin_base.js')
+var manifest = require('./manifest.json')
 
 var ExamplePlugin = {
-    name: "Example",
-    description: "An example plugin for a service provider",
+    name: manifest.plugin_name,
+    description: manifest.plugin_description,
 
-    api_key: "",
-    api_secret: "",
+    /* This bit isn't right - the plugin will actually need to query the DB
+    with the table name '`manifest.plugin_uid`_data'
+    */
+    api_key: manifest.data_fields.custom['api_key'],
+    api_secret: manifest.data_fields.custom['api_key'],
 
+    /* Implement the functions required by `plugin_base`:
+    init, get_message_status, send_message, shutdown
+    */
+
+    init: function(){
+        /* This is called by voluble when first initializing the message-sending plugin.
+        Plugins should scrutinize provided parameters and make sure that is possible to safely
+        init the plugin, then do so.
+        If the plugin can be initialized, then `init` should return an instance of the plugin.
+        If not, then return None. */
+    },
+    
     send_message: function (message) {
-        // This must be defined by *every* function. It will be called by the voluble when a message needs to be sent.
-        console.log("Sending the message: " + message).
+        // This is called by voluble when a plugin needs to be sent.
+        console.log("Sending the message: " + message)
         return voluble_plugin_base.message_states.MESSAGE_SENT
     }
 }
--- bin/plugins/plugin_base.js
+++ bin/plugins/plugin_base.js
@@ -1,15 +1,28 @@
-// @ts-check
 var errors = require('common-errors')
 var message = require('../messages/message')
 
 var voluble_plugin = {
-    name: 'plugin_base_name',
-    description: 'plugin_base_description',
-
-    send_message: function (message_content, contact) {
-        // This absolutely must be overridden by a plugin
-        throw new errors.NotImplementedError('Plugin ' + this.name + ' has not defined a message-sending method. Contact the plugin author for a fix.');
-
-    }
+    name: null,
+    description: null,
+    plugin_uid: null,
 }
-module.exports = {voluble_plugin, message.message_states};
+
+voluble_plugin.init = function(){
+    throw new errors.NotImplementedError("Plugin " + this.name + " has not defined the function 'init'. Contact the plugin author for a fix.")
+}
+
+voluble_plugin.shutdown = function(){
+    throw new errors.NotImplementedError("Plugin " + this.name + "has not defined the function 'shutdown'. Contact the plugin author for a fix.")
+}
+
+voluble_plugin.get_message_status = function(message_id){
+    throw new errors.NotImplementedError("Plugin " + this.name + "has not defined the function 'get_message_status'. Contact the plugin author for a fix.")
+}
+
+voluble_plugin.send_message = function (message_content, contact) {
+    // This absolutely must be overridden by a plugin
+    throw new errors.NotImplementedError('Plugin ' + this.name + ' has not defined a message-sending method. Contact the plugin author for a fix.');
+
+}
+
+module.exports = {voluble_plugin};
