23bc04bfa453bb05c6a3fdd635b2a15984168776
Cal McLean <calmcl1@aol.com>
2017-10-04 14:56:47 +0100

    JSDoc Style

--- bin/plugins/plugin_base.js
+++ bin/plugins/plugin_base.js
@@ -7,22 +7,28 @@ var voluble_plugin = {
     plugin_uid: null,
 }
 
+/**
+ * This is called by voluble when first initializing the message-sending plugin.
+ * Plugins should scrutinize provided parameters and make sure that is possible to safely
+ * init the plugin, then do so.
+ * If the plugin can be initialized, then `init` should return an instance of the plugin.
+ * If not, then return None.
+ */
 voluble_plugin.init = function () {
-    /* This is called by voluble when first initializing the message-sending plugin.
-    Plugins should scrutinize provided parameters and make sure that is possible to safely
-    init the plugin, then do so.
-    If the plugin can be initialized, then `init` should return an instance of the plugin.
-    If not, then return None. */
     throw new errors.NotImplementedError("Plugin " + this.name + " has not defined the function 'init'. Contact the plugin author for a fix.")
 }
 
+/**
+ * This is called by voluble when unloading a plugin (such as when voluble is shutting down.)
+ */
 voluble_plugin.shutdown = function () {
-    // This is called by voluble when unloading a plugin (such as when voluble is shutting down.)
     throw new errors.NotImplementedError("Plugin " + this.name + "has not defined the function 'shutdown'. Contact the plugin author for a fix.")
 }
 
+/**
+ * This is called by voluble when a message needs to be sent.
+ */
 voluble_plugin.send_message = function (message_content, contact) {
-    // This is called by voluble when a message needs to be sent.`
     throw new errors.NotImplementedError('Plugin ' + this.name + ' has not defined a message-sending method. Contact the plugin author for a fix.');
 }
 
