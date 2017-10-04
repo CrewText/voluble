b84b8d7a3dc795d26ed7fed08b16961e99e0c304
Cal McLean <calmcl1@aol.com>
2017-10-04 14:54:53 +0100

    Removed views

--- app.js
+++ app.js
@@ -73,11 +73,6 @@ var server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -110,7 +105,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
