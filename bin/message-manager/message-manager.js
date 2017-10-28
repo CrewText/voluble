bb13aa255d64338b026e3c1d888819afcfc6fe51
Cal McLean <calmcl1@aol.com>
2017-10-28 17:31:05 +0100

    message_states are now a property of MessageManager

--- bin/message-manager/message-manager.js
+++ bin/message-manager/message-manager.js
@@ -1,15 +1,47 @@
 const winston = require('winston')
-const messages = require('../messages/message')
+const Q = require('Q')
+const message = require('../messages/message')
+const utils = require('../utilities')
 
 var MessageManager = {
+    message_states = Object.freeze(
+        {
+            MESSAGE_FAILED: -1,
+            MESSAGE_UNSENT: 0,
+            MESSAGE_SENT: 1,
+            MESSAGE_DELIVERED_SERVICE: 2,
+            MESSAGE_DELIVERED_USER: 3,
+            MESSAGE_READ: 4,
+            MESSAGE_REPLIED: 5
+        }
+    ),
+
+    message_directions = Object.freeze({
+        DIRECTION_INBOUND: 0,
+        DIRECTION_OUTBOUND: 1
+    })
 
 }
 
 MessageManager.createEmptyMessage = function () {
-    /* TODO: Should createNewMessage return a ready-to-go message with a new DB message ID?
-     Should it register in the DB? */
-    let m = Object.create(messages.Message)
+    let m = Object.create(message.Message)
     return m
-  }
+}
+
+MessageManager.createNewMessage = function (msg_body, msg_contact_id, msg_direction, msg_servicechain, msg_is_reply_to = null) {
+    /* TODO: Should createNewMessage return a ready-to-go message with a new DB message ID?
+ Should it register in the DB? */
+    let m = this.createEmptyMessage()
+    m.state = this.message_states.MESSAGE_UNSENT,
+        m.body = msg_body
+    // TODO: Make this resolve to an actual contact object?
+    m.contact = msg_contact_id
+    // TODO: Make this resolve to an actual servicechain object?
+    m.servicechain = msg_servicechain,
+        m.direction = msg_direction,
+        m.is_reply_to = msg_is_reply_to
+
+    return m
+}
 
 module.exports = MessageManager
--- bin/messages/message.js
+++ bin/messages/message.js
@@ -1,21 +1,10 @@
-var message_states = Object.freeze(
-  {
-    MESSAGE_FAILED: -1,
-    MESSAGE_SENT: 1,
-    MESSAGE_DELIVERED_SERVICE: 2,
-    MESSAGE_DELIVERED_USER: 3,
-    MESSAGE_READ: 4,
-    MESSAGE_REPLIED: 5
-  }
-)
-
 var Message = {
   id: null,
   body: null,
   servicechain: null,
   state: null,
-  contact: null
+  contact: null,
+  direction: null,
+  is_reply_to: null
 }
-
-exports.message_states = message_states
 exports.Message = Message
