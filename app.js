ef397cef3bab94ef98f1c43b9f841b24a5af4283
Cal McLean <calmcl1@aol.com>
2017-11-26 17:03:10 +0000

    Create a test form to generate a new message

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ server.on('error', onError);
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 //app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
