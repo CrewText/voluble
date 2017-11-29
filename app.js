4deaeaea17b94fe1aa414e66bc808c18ee9446b9
Cal McLean <calmcl1@aol.com>
2017-11-29 15:10:50 +0000

    Removed some unnecessary packages

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // @ts-check
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
-const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const winston = require('winston')
 winston.level = 'debug'
@@ -88,7 +86,6 @@ server.on('error', onError);
 //app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
