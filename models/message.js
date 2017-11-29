8f3fda477ffadfd562a2a27e253e248ce0e8bae3
Cal McLean <calmcl1@aol.com>
2017-11-29 16:34:15 +0000

    Added ID for message model

--- models/message.js
+++ models/message.js
@@ -1,6 +1,11 @@
 let message_model = function (sequelize, DataTypes) {
     var Message = sequelize.define('Message', {
 
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
         message_body: DataTypes.STRING(1024),
         servicechain: DataTypes.INTEGER,
         contact: DataTypes.BIGINT,
