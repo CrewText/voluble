60dd715d620280de9ec0d6e15ea54374bb59eb2d
Cal McLean <calmcl1@aol.com>
2017-11-29 16:31:03 +0000

    Added basic model for Message

--- models/message.js
+++ models/message.js
@@ -1,6 +1,20 @@
-let message_model = function(sequelize, DataTypes){
-    var Message = sequelize.define('Message',{
-        message_body: DataTypes.STRING
+let message_model = function (sequelize, DataTypes) {
+    var Message = sequelize.define('Message', {
+
+        message_body: DataTypes.STRING(1024),
+        servicechain: DataTypes.INTEGER,
+        contact: DataTypes.BIGINT,
+        is_reply_to: DataTypes.BIGINT,
+        direction: DataTypes.BOOLEAN,
+        message_state: DataTypes.ENUM('MSG_PENDING',
+            'MSG_SENDING',
+            'MSG_SENT',
+            'MSG_DELIVERED_SERVICE',
+            'MSG_DELIVERED_USER',
+            'MSG_READ',
+            'MSG_REPLIED',
+            'MSG_FAILED')
+
     })
 
     return Message
