4dbb019f489b1724dad017c92a4a0316ba6bdab4
Cal McLean <calmcl1@aol.com>
2017-12-20 16:41:12 +0000

    loadAllPlugins does technically work, but only with the array bodge

--- bin/plugin-manager/plugin-manager.js
+++ bin/plugin-manager/plugin-manager.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path');
 const winston = require('winston')
 const Promise = require('bluebird')
 
@@ -7,14 +8,62 @@ var PluginManager = {
     totalPluginsList: [],
     availablePlugins: [],
 
-    getPluginById: function (id){
+    getPluginById: function (id) {
         // TODO: Implement getPluginFromID properly
-        return Promise.try(function(){
+        return Promise.try(function () {
             //return PluginManager.availablePlugins[id]
-            if (id == 0){
-                return PluginManager.availablePlugins[0]}
-            else{throw new Error("Plugin with id "+id+" does not exist")}
+            if (id == 0) {
+                return PluginManager.availablePlugins[0]
+            }
+            else { throw new Error("Plugin with id " + id + " does not exist") }
         })
+    },
+
+    /**
+    * For all of the existent plugins in the plugin directory, try and init each one
+    * If this is successful, add the plugin to the list `availablePLugins`.
+    */
+
+    loadAllPlugins: function () {
+        // Cycle through plugin directory and try to init all available plugins
+        // FIXME: make loadAllPlugins actually work, and loop through stuff! #1
+
+        /**
+         * Logic - the plugin's directory is something we should be able to identify it by.
+         * SO: We should loop through all of the directories under the plugin directory and see if a plugin exists in the database with that directory.
+         * If so, update it's status to not initialized. Otherwise, create it as not initialized.
+         * The point of this is so that when plugins are added, they aren't removed and re-added on startup, which could likely lead to a change in ID number in the database when a new plugin is added or an existing one removed.
+         */
+
+        // Get a list of all of the directories under the plugin directory
+        let plugin_subdirs = fs.readdirSync(PluginManager.plugin_dir).filter(function (element) {
+            let plugin_subdir_fullpath = path.join(PluginManager.plugin_dir, element)
+
+            if (fs.statSync(plugin_subdir_fullpath).isDirectory()) { return true }
+            else { return false }
+        })
+
+        winston.debug("Found plugins at:\n\t" + plugin_subdirs)
+
+        // Loop through each directory and try to init a plugin if there is one
+        plugin_subdirs.forEach(function (plugin_subir_rel) {
+            let plugin_file_abs = path.join(PluginManager.plugin_dir, plugin_subir_rel, "plugin.js")
+            if (fs.existsSync(plugin_file_abs)) {
+                try {
+                    let plug = require(plugin_file_abs)
+                    let plug_obj = plug.createPlugin()
+                    winston.info("Loaded plugin:\n\t" + plug_obj.name)
+                    PluginManager.totalPluginsList.push(plug_obj)
+                    plug_obj.init()
+                    PluginManager.availablePlugins.push(plug_obj)
+                } catch (err) {
+                    winston.error("Failed to load plugin: " + plugin_file_abs + "\nMessage: " + err.message)
+                }
+            } else {
+                winston.info("No plugin in\n\t" + path.join(PluginManager.plugin_dir, plugin_subir_rel))
+            }
+        })
+
     }
 }
 
@@ -26,36 +75,6 @@ PluginManager.initAllPlugins = function (plugin_dir) {
     winston.debug("Attempting to load plugins")
     this.plugin_dir = plugin_dir
     this.loadAllPlugins()
-}
-
-/**
- * For all of the existent plugins in the plugin directory, try and init each one
- * If this is successful, add the plugin to the list `availablePLugins`.
- */
-PluginManager.loadAllPlugins = function () {
-    // Cycle through plugin directory and try to init all available plugins
-    // FIXME: make loadAllPlugins actually work, and loop through stuff! #1
-    let esendexPlugin = require("../plugins/esendex/plugin")
-    let plug = esendexPlugin.createPlugin()
-    this.availablePlugins.push(plug)
-    plug.init()
-
-
-    /**
-     * Logic - the plugin's directory is something we should be able to identify it by.
-     * SO: We should loop through all of the directories under the plugin directory and see if a plugin exists in the database with that directory.
-     * If so, update it's status to not initialized. Otherwise, create it as not initialized.
-     * The point of this is so that when plugins are added, they aren't removed and re-added on startup, which could likely lead to a change in ID number in the database when a new plugin is added or an existing one removed.
-     */
-
-    /*try {
-        this.availablePlugins.forEach(function (plugin) {
-            winston.debug(plugin.createPlugin().init())
-        });
-    } catch (e) {
-        console.log(e)
-    }
-    */
 }
 
 
