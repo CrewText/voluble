f221b06bd3a4932e9fd2c18cee1b56b6a2ea0a9f
Cal McLean <calmcl1@aol.com>
2017-12-20 17:30:31 +0000

    Create PluginInitFailedError

---
+++ bin/voluble-errors/PluginInitFailedError.js
@@ -0,0 +1,10 @@
+function PluginInitFailedError(message) {
+    this.message = message
+    this.name = "PluginInitFailedError"
+    Error.captureStackTrace(this, PluginInitFailedError)
+}
+
+PluginInitFailedError.prototype = Object.create(Error.prototype)
+PluginInitFailedError.constructor = PluginInitFailedError
+
+module.exports = PluginInitFailedError
