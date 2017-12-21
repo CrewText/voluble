86bcee1308ec666e827ea3acd1f0cc7452b94e8f
Cal McLean <calmcl1@aol.com>
2017-12-21 16:14:43 +0000

    Added new MessageFailedError

---
+++ bin/voluble-errors/MessageFailedError.js
@@ -0,0 +1,10 @@
+function MessageFailedError(message) {
+    this.message = message
+    this.name = "MessageFailedError"
+    Error.captureStackTrace(this, MessageFailedError)
+}
+
+MessageFailedError.prototype = Object.create(Error.prototype)
+MessageFailedError.constructor = MessageFailedError
+
+module.exports = MessageFailedError
