c24b22b05557abb0c9fa9e60fe78f1db8d102a50
calmcl1 <calmcl1@aol.com>
2017-12-30 17:27:34 +0000

    Added function to get all services

--- routes/services.js
+++ routes/services.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Promise = require('bluebird')
+const winston = require('winston')
+const pluginManager = require('../bin/plugin-manager/plugin-manager')
+
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.get('/', function(req,res,next){
-  res.render('services_list')
+  pluginManager.getAllPlugins()
+  .then(function(rows){
+    res.status(200).json(rows)
+  })
+  .catch(function(err){
+    res.status(500).json(err)
+    winston.error(err)
+  })
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
