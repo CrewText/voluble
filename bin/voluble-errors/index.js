7c8cc0535b8abc24077d064ab3e39daa5235e5f8
calmcl1 <calmcl1@aol.com>
2018-01-02 20:53:04 +0000

    Added PluginFailedToSendError

--- bin/voluble-errors/index.js
+++ bin/voluble-errors/index.js
@@ -2,11 +2,13 @@ const MessageAlreadySentError = require('./MessageAlreadySentError')
 const MessageFailedError = require('./MessageFailedError')
 const PluginDoesNotExistError = require('./PluginDoesNotExistError')
 const PluginInitFailedError = require('./PluginInitFailedError')
+const PluginFailedToSendError = require('./PluginFailedToSendError')
 let Errors = {
     MessageAlreadySentError,
     MessageFailedError,
     PluginDoesNotExistError,
     PluginInitFailedError,
+    PluginFailedToSendError
 }
 
 module.exports = Errors
