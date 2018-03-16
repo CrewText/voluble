3086b739e5e95b38dc604f2b71056f479acd5c0f
calmcl1 <calmcl1@aol.com>
2018-03-16 10:42:34 +0000

    Added extra comments to explain the role of the user manager

--- src/bin/user-manager/index.ts
+++ src/bin/user-manager/index.ts
@@ -8,6 +8,11 @@ import * as utils from '../../utilities'
 import { UserInstance } from '../../models/user';
 import { Auth0Manager } from './auth0-manager'
 
+/**
+ * The UserManager exists in order to co-ordinate the functions regarding Voluble users, and
+ * constructing full user profiles from the information stored in the Voluble database and extra
+ * PII stored in the Auth0 database, both in cleartext and encrypted.
+*/
 export namespace UserManager {
 
     export interface IUserMetadata {
@@ -45,13 +50,22 @@ export namespace UserManager {
         return JSON.parse(decr_string)
     }
 
-    export function getUserMetadataByID(id: string): Promise<IUserMetadata> {
-        return utils.verifyNumberIsInteger(id)
+    // The distinction here is between user details (which might represent something approaching a full profile),
+    // which is a combination of the Auth0 user profile, the decrypted Auth0 user metadata, and the Voluble database
+    // information.
+
+    /**
+     * Returns the full decrypted metadata stored by Auth0 - this is the PII not covered by Auth0's basic Profile,
+     * such as user role, and so on.
+     * @param vol_user_id Voluble user ID
+     */
+    export function getUserMetadataByID(vol_user_id: string): Promise<IUserMetadata> {
+        return utils.verifyNumberIsInteger(vol_user_id)
             .then(function (user_id) {
                 return db.User.findById(user_id)
             })
             .then(function (user_inst) {
-                if (!user_inst) { return Promise.reject(new errs.NotFoundError("Could not find user with ID " + id)) }
+                if (!user_inst) { return Promise.reject(new errs.NotFoundError("Could not find user with ID " + vol_user_id)) }
 
                 return Auth0Manager.getEncryptedUserMetadataByID(user_inst.auth0_id)
                     .then(function (encrypted_details: any) {
