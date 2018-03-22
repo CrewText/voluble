82ee122ad7e1c67e59bb94a81310fc6492770917
calmcl1 <calmcl1@aol.com>
2018-03-22 19:04:27 +0000

    Removed unnecessary data fields from User model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,13 +1,8 @@
 import * as Sequelize from "sequelize"
 
 export interface UserAttributes {
-    first_name: string,
-    surname: string,
-    email_address: string,
-    phone_number: string,
     auth0_id: string,
-    auth0_encr_iv: string,
-    org_number: number
+    org_number: number | null
 }
 
 export interface UserInstance extends Sequelize.Instance<UserAttributes>{
@@ -15,13 +10,8 @@ export interface UserInstance extends Sequelize.Instance<UserAttributes>{
     createdAt: Date,
     updatedAt: Date,
 
-    first_name: string,
-    surname: string,
-    email_address: string,
-    phone_number: string,
     auth0_id: string,
-    auth0_encr_iv: string
-    org_member: number
+    org_member: number | null
 }
 
 export default function (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes) {
@@ -31,12 +21,7 @@ export default function (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.Da
             primaryKey: true,
             autoIncrement: true,
         },
-        first_name: DataTypes.STRING,
-        surname: DataTypes.STRING,
-        email_address: DataTypes.STRING,
-        phone_number: DataTypes.STRING,
         auth0_id: DataTypes.STRING,
-        auth0_encr_iv: DataTypes.STRING,
         org_number: DataTypes.BIGINT
     })
 
