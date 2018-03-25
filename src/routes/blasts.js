7fbc70c68905ebe2c8749687d9825755c969076f
calmcl1 <calmcl1@aol.com>
2018-03-25 13:46:49 +0100

    Corrected errors that stopped tsc from compiling boilerplate routes

--- src/routes/blasts.js
+++ src/routes/blasts.js
@@ -3,17 +3,14 @@ var router = express.Router();
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.get('/', function(req,res,next){
-  res.render('blasts_list')
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.get('/{id}', function(req, res, next){
-  res.render('blasts_info', {contact_id: id})
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.post('/', function(req, res, next){
-  res.render('blasts_send', {data: req.params}) // Is this right?
 })
 
 module.exports = router;
--- src/routes/groups.js
+++ src/routes/groups.js
@@ -3,27 +3,22 @@ var router = express.Router();
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.get('/', function(req,res,next){
-  res.render('groups_list')
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.get('/{id}', function(req, res, next){
-  res.render('groups_list_contacts', {id: id})
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
 router.post('/', function(req, res, next){
-  res.render('groups_create', {data: req.params}) // Is this right?
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.put('/{id}', function(req, res, next){
-  res.render('groups_update', {group_id: id, data: req.params}) // Is this right?
+router.put('/:id', function(req, res, next){
 })
 
 /* Note: this is boilerplate and has NOT been implemented yet */
-router.delete('/{id}', function(req, res, next){
-  res.render('groups_delete', {group_id: id})
+router.delete('/:id', function(req, res, next){
 })
 
 module.exports = router;
