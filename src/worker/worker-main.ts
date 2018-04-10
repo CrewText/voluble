081d0ad8e52c3ed3e256b4947e11605f1c4b8b79
calmcl1 <calmcl1@aol.com>
2018-04-10 18:03:32 +0100

    Added workers flow

--- src/worker/worker-main.ts
+++ src/worker/worker-main.ts
@@ -1,23 +1,40 @@
 const winston = require('winston')
+if (!process.env.IS_PRODUCTION) {
+    winston.info("Detected dev environment")
+    winston.level = 'debug'
+} else {
+    winston.info("Detected prod environment")
+    winston.level = 'info'
+}
 import * as rsmqWorker from 'rsmq-worker'
 import * as redis from 'redis'
 
 winston.info("Worker: Initializing worker process")
 
 let client;
-    if (process.env.REDISTOGO_URL){
-        let rtg   = require("url").parse(process.env.REDISTOGO_URL);
-        client = redis.createClient(rtg.port, rtg.hostname)
-        client.auth(rtg.auth.split(":")[1]);
-    } else {
-        client = redis.createClient({host:"192.168.56.104"})
-    }
-let worker = new rsmqWorker("messages", { redis: client })
+if (process.env.REDISTOGO_URL) {
+    let rtg = require("url").parse(process.env.REDISTOGO_URL);
+    client = redis.createClient(rtg.port, rtg.hostname)
+    client.auth(rtg.auth.split(":")[1]);
+} else {
+    client = redis.createClient({ host: "192.168.56.104" })
+}
 
-worker.on("message", function (message, next, message_id) {
+
+let worker_msg_recv = new rsmqWorker("message-send", { redis: client })
+let worker_send_msg_update = new rsmqWorker("message-state-update", { redis: client })
+
+worker_msg_recv.on("message", function (message, next, message_id) {
     let parsed_msg = JSON.parse(message)
     winston.info(`Got message: ${parsed_msg.body}`)
+    let status = { message: parsed_msg.id, status: "MSG_SENDING" }
+    worker_send_msg_update.send(JSON.stringify(status))
     next()
 })
 
-worker.start()
+if (worker_msg_recv.start()){
+    winston.info("Inited message-receive worker")
+} else {
+    winston.error("Failed to init message-receive worker")
+    
+}
