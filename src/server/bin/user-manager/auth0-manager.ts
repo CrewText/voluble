314312f698f8aea26365c7e4a4111e8024f9badd
calmcl1 <calmcl1@aol.com>
2018-05-01 10:31:14 +0100

    Remove redundant Auth0Profile types

--- src/server/bin/user-manager/auth0-manager.ts
+++ src/server/bin/user-manager/auth0-manager.ts
@@ -16,43 +16,9 @@ export namespace Auth0Manager {
         phone_number: string,
     }
 
-    /**
-     * Auth0ProfileAppMetadata is a representation of the App Metadata field available in a given Auth0 profile.
-     * It is intended to be used for data that the user has no access to.
-     */
-    export interface Auth0ProfileAppMetadata {
-        role: "user:contact" | "organization:author" | "organization:manager" | "organization:admin" | "voluble:admin",
-        organization: number
-    }
+    export type Role =  "user:contact" | "organization:author" | "organization:manager" | "organization:admin" | "voluble:admin",
 
-    export interface Auth0Profile {
-        app_metadata: Auth0ProfileAppMetadata,
-        blocked: boolean | void | null,
-        created_at: Date,
-        email: string,
-        email_verified: boolean,
-        identities: Object | any,
-        multifactor: string[] | void | null,
-        last_ip: string | void | null,
-        last_login: Date | void | null,
-        logins_count: number | void | null,
-        name: string,
-        nickname: string,
-        phone_number: string,
-        phone_verified: boolean,
-        picture: string,
-        updated_at: Date,
-        user_id: string,
-        user_metadata: Auth0ProfileUserMetadata,
-        username: string
-    }
-
-    interface CCAccessToken {
-        access_token: string,
-        token_type: string,
-        expires_in: number,
-        [key: string]: any
-    }
+    export type Auth0Profile = auth0.User
 
     let auth0MgmtClient = new auth0.ManagementClient({
         domain: <string>process.env.AUTH0_DOMAIN,
