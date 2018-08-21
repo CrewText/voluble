d0858615183143b4f709fad31f93dc3656f8c6fd
calmcl1 <calmcl1@aol.com>
2018-08-21 16:36:36 +0100

    verifyNumberIsInteger now throws more meaningful errors

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,6 +2,7 @@ import * as Promise from "bluebird"
 import { isNumber } from "util";
 import * as isInt from "validator/lib/isInt"
 import * as toInt from "validator/lib/toInt"
+const errs = require('common-errors')
 
 /**
  * Confirms that the supplied ID is a valid number.
@@ -13,14 +14,14 @@ export function verifyNumberIsInteger(id: string | number): Promise<number> {
 
   if (typeof id == "number") {
     let int_num = toInt(id.toString(), 10)
-    if (!int_num) { return Promise.reject(`${id} is not an integer`) }
+    if (!int_num) { return Promise.reject(new errs.TypeError(`${id} is not an integer`)) }
     return Promise.resolve(int_num)
   }
-  else{
-    if (isInt(id)){
+  else {
+    if (isInt(id)) {
       return Promise.resolve(toInt(id))
     }
-    return Promise.reject(`${id} is not an integer`)
+    return Promise.reject(new errs.TypeError(`${id} is not an integer`))
   }
 
 }
