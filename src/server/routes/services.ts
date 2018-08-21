051bd73d715c720be025be77d36447f9193e4222
calmcl1 <calmcl1@aol.com>
2018-08-21 20:38:43 +0100

    /services now uses JSend

--- src/server/routes/services.ts
+++ src/server/routes/services.ts
@@ -2,32 +2,32 @@ import * as express from 'express'
 const router = express.Router();
 import * as Promise from 'bluebird'
 const winston = require('winston')
-import {PluginManager} from '../../plugin-manager'
+import { PluginManager } from '../../plugin-manager'
 import * as utils from '../../utilities'
 
-router.get('/', function(req,res,next){
+router.get('/', function (req, res, next) {
   PluginManager.getAllServices()
-  .then(function(rows){
-    res.status(200).json(rows)
-  })
-  .catch(function(err){
-    res.status(500).json(err)
-    winston.error(err)
-  })
+    .then(function (rows) {
+      res.jsend.success(rows)
+    })
+    .catch(function (error: any) {
+      res.jsend.error(error.message)
+      //res.status(500).send(error.message)
+    })
 })
 
-router.get('/:service_id', function(req, res, next){
+router.get('/:service_id', function (req, res, next) {
   utils.verifyNumberIsInteger(req.params.service_id)
-  .then(function(service_id){
-    return PluginManager.getServiceById(service_id)
-  })
-  .then(function(plugin){
-    res.status(200).json(plugin)
-  })
-  .catch(function(err){
-    res.status(500).json(err)
-    winston.error(err)
-  })
+    .then(function (service_id) {
+      return PluginManager.getServiceById(service_id)
+    })
+    .then(function (service) {
+      if (service) { res.jsend.success(service) }
+    })
+    .catch(function (error: any) {
+      res.jsend.error(error.message)
+      //res.status(500).send(error.message)
+    })
 })
 
 module.exports = router;
