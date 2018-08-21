96560ab1eb8020de5b377234095959872ce9ae9d
calmcl1 <calmcl1@aol.com>
2018-08-21 15:44:42 +0100

    Move the message-state-update worker start into a separate function, only called by the main server

--- src/queue-manager/index.ts
+++ src/queue-manager/index.ts
@@ -9,6 +9,7 @@ import * as rsmqWorker from 'rsmq-worker'
 const errs = require('common-errors')
 
 export namespace QueueManager {
+
     let client: redis.RedisClient;
     if (process.env.REDISTOGO_URL) {
         let rtg = require("url").parse(process.env.REDISTOGO_URL);
@@ -18,12 +19,6 @@ export namespace QueueManager {
         client = redis.createClient()//{ host: "127.0.0.1" })
     }
 
-    //let queue = kue.createQueue({redis:{createClientFactory: function(){return client}}})
-    // let queue = kue.createQueue({ redis: process.env.REDISTOGO_URL || "redis://192.168.56.104" })
-    // queue.on('error', function (err) {
-    //     winston.error(err)
-    // })
-
     let rsmq = new RedisSMQ({ client: client })
 
     if (rsmq.redis.server_info.redis_version) {
@@ -32,28 +27,22 @@ export namespace QueueManager {
         winston.error("QM: Failed to connect to redis server")
     }
 
-    createQueues()
+    export function init_queues(): void {
+        createQueues()
 
-    let worker_send_msg_update = new rsmqWorker("message-state-update", { redis: client })
-    worker_send_msg_update.on("message", function (message, next, message_id) {
-        let update = JSON.parse(message)
-        winston.debug("QM: Got message update for message " + update.message_id + ": " + update.status)
-        MessageManager.updateMessageState(update.message_id, update.status)
-            .catch(errs.NotFoundError, function (error) {
-                winston.info("QM: Dropping message update request for message with ID " + update.message_id)
-            })
-            .then(function () { next() })
-    })
-    worker_send_msg_update.start()
+        let worker_send_msg_update = new rsmqWorker("message-state-update", { redis: client })
+        worker_send_msg_update.on("message", function (message, next, message_id) {
+            let update = JSON.parse(message)
+            winston.debug("QM: Got message update for message " + update.message_id + ": " + update.status)
+            MessageManager.updateMessageState(update.message_id, update.status)
+                .catch(errs.NotFoundError, function (error) {
+                    winston.info("QM: Dropping message update request for message with ID " + update.message_id)
+                })
+                .then(function () { next() })
+        })
+        worker_send_msg_update.start()
 
-    // queue.process('message-state-update', function (job, done) {
-    //     winston.debug("QM: Got message update for message " + job.data.message_id + ": " + job.data.status)
-    //     MessageManager.updateMessageState(job.data.message_id, job.data.status)
-    //         .catch(errs.NotFoundError, function (error) {
-    //             winston.info("QM: Dropping message update request for message with ID " + job.data.message_id)
-    //         })
-    //         .finally(function () { done() })
-    // })
+    }
 
     export function addMessageToSendRequest(message: MessageInstance) {
         winston.debug("QM: Sending message with ID " + message.id)
@@ -70,17 +59,6 @@ export namespace QueueManager {
                 throw err
             }
         })
-
-        // let job = queue.create('message-send', JSON.stringify(message)).save(function (error: any) {
-        //     if (!error) {
-        //         winston.info("QM: Added send request for message " + message.id)
-        //         winston.debug("QM: Job ID: " + job.id)
-        //     } else {
-        //         winston.error(error)
-        //         throw error
-        //     }
-        // })
-
     }
 
     export function addMessageStateUpdateRequest(message_id: number, message_state: string) {
@@ -95,19 +73,6 @@ export namespace QueueManager {
                 winston.error(err)
                 throw err
             }
-            // })
-
-            // let job = queue.create('message-state-update', {
-            //     message_id: message_id,
-            //     status: message_state
-            // }).save(function (error: any) {
-            //     if (!error) {
-            //         return true
-            //     } else {
-            //         winston.error(error)
-            //         throw error
-            //     }
-            // })
         })
     }
 
