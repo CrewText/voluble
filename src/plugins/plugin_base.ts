3277ecdf5e81f0183f8a11697469af768442b539
calmcl1 <calmcl1@aol.com>
2018-08-29 23:46:18 +0200

    Plugins will set up their data stores on init, based on manifest.object_data contents

--- src/plugins/plugin_base.ts
+++ src/plugins/plugin_base.ts
@@ -1,23 +1,23 @@
 var errors = require('common-errors')
-//var voluble_errors = require('../../bin/voluble-errors')
 import * as db from '../models'
 import * as events from 'events'
 import * as Promise from 'bluebird'
 export type contactInstance = db.ContactInstance
 export type messageInstance = db.MessageInstance
 
+interface ObjectData {
+    message?: string[],
+    organization?: string[],
+    user?: string[],
+    contact?: string[]
+}
+
 interface Manifest {
     plugin_name: string,
     plugin_description: string,
     data_tables?: Object,
+    object_data?: ObjectData,
     npm_modules?: string[]
-}
-
-interface ObjectData {
-    Message?: any,
-    Organization?: any,
-    User?: any,
-    Contact?: any
 }
 
 /**
@@ -60,8 +60,54 @@ export class voluble_plugin implements IVolublePluginBase {
             this.data_tables = manifest["data_tables"]
         }
 
-        // insert information into data_tables
+        // insert information into object_data
+        if (manifest.object_data) {
+            let db_define_proms: Promise<any>[] = []
 
+            Object.keys(manifest.object_data).forEach(object_type => {
+                let cols = {
+                    id: {
+                        type: db.models.Sequelize.INTEGER,
+                        primaryKey: true,
+                    }
+                }
+
+                manifest.object_data[object_type].forEach(attr => {
+                    cols[attr] = db.models.Sequelize.STRING
+                });
+
+                let table_name = `pl_${this.name.replace(' ', '')}_${object_type}`
+
+                // For the sake of convenience, since we know that our table is referencing a particulary entry in a table
+                // that alreasy exists, we can set up the foreign keys too. This will help us populate the `plugin.object_data` field later.
+                let related_model
+                switch (object_type) {
+                    case "message":
+                        related_model = db.models.Message
+                        break
+                    case "contact":
+                        related_model = db.models.Contact
+                        break
+                    case "organization":
+                        related_model = db.models.Organization
+                        break
+                    case "user":
+                        related_model = db.models.User
+                        break
+                }
+
+                let model = db.sequelize.define(table_name, cols)
+                if (related_model) {
+                    related_model.hasOne(model, {foreignKey: "object_id" })
+                }
+                let prom = model.sync()
+                db_define_proms.push(prom)
+            });
+
+            Promise.all(db_define_proms)
+
+            // We should actually populate these fields now...
+        }
     }
 
     send_message(message: db.MessageInstance, contact: db.ContactInstance): Promise<boolean> {
