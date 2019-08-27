b0d2c9c1b381948599d15b0b52d638151013f098
calmcl1 <calmcl1@aol.com>
2019-08-27 17:34:31 +0100

    Fixed issue where TelegramPlugin didn't export a creation method; created mock functions for telegram methods

--- src/plugins/telegram/plugin.ts
+++ src/plugins/telegram/plugin.ts
@@ -10,14 +10,16 @@ class TelegramPlugin extends plugin_base.voluble_plugin {
     }
 
     handle_incoming_message(message_data: any): plugin_base.InterpretedIncomingMessage {
-
+        throw new Error(`In plugin ${this.name} - the method 'handle_incoming_message' has not been implemented!`)
     }
 
     send_message(message: plugin_base.messageInstance, contact: plugin_base.contactInstance): boolean {
-        rp.post(this.TELEGRAM_SERVER_ADDR)
+        //rp.post(this.TELEGRAM_SERVER_ADDR)
+        throw new Error(`In plugin ${this.name} - the method 'send_message' has not been implemented!`)
     }
 }
 
 var createPlugin = function () {
     return new TelegramPlugin()
 }
+module.exports = createPlugin
