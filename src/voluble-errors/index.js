57256ac20f77f6859afcdb03b28c78aaf72d46c7
Cal McLean <calmcl1@aol.com>
2019-11-29 10:03:05 +0000

    Add extra errors, start to move away from common-errors

--- src/voluble-errors/index.js
+++ src/voluble-errors/index.js
@@ -3,12 +3,16 @@ const MessageFailedError = require('./MessageFailedError')
 const PluginDoesNotExistError = require('./PluginDoesNotExistError')
 const PluginInitFailedError = require('./PluginInitFailedError')
 const PluginFailedToSendError = require('./PluginFailedToSendError')
+class ResourceNotFoundError extends Error { }
+class InvalidParameterValueError extends Error { }
 let Errors = {
     MessageAlreadySentError,
     MessageFailedError,
     PluginDoesNotExistError,
     PluginInitFailedError,
-    PluginFailedToSendError
+    PluginFailedToSendError,
+    ResourceNotFoundError,
+    InvalidParameterValueError
 }
 
 module.exports = Errors
