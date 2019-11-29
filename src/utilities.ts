8ad12a0330b7035aaa69f8474613bd8e7bbf2b20
Cal McLean <calmcl1@aol.com>
2019-11-29 10:03:39 +0000

    Get rid of the old verifyNumberIsInteger, use validator

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,31 +1,6 @@
-import * as Promise from "bluebird";
+// import * as Promise from "bluebird";
 import { PhoneNumberFormat, PhoneNumberUtil } from 'google-libphonenumber';
-import * as isInt from "validator/lib/isInt";
-import * as toInt from "validator/lib/toInt";
 const errs = require('common-errors')
-
-/**
- * Confirms that the supplied ID is a valid number.
- * @param {string} id String to confirm is a valid integer
- * @returns {promise} containing value of the ID number as integer
- */
-
-export function verifyNumberIsInteger(id: string | number): Promise<number> {
-
-  if (id === 0 || id === "0") { return Promise.resolve(0) }
-  if (typeof id == "number") {
-    let int_num = toInt(id.toString(), 10)
-    if (!int_num) { return Promise.reject(new errs.TypeError(`${id} is not an integer`)) }
-    return Promise.resolve(int_num)
-  }
-  else {
-    if (isInt(id)) {
-      return Promise.resolve(toInt(id))
-    }
-    return Promise.reject(new errs.TypeError(`${id} is not an integer`))
-  }
-
-}
 
 
 /**
@@ -44,5 +19,3 @@ export function getE164PhoneNumber(phone_number: string): string {
   }
 
 }
-
-//module.exports.verifyNumberIsInteger = verifyNumberIsInteger
