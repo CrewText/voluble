96a53019d14e2354d99643c967f940d6cb8a59a2
Cal McLean <calmcl1@aol.com>
2020-11-16 16:44:03 +0000

    all the updates

--- src/server/security/jwt.ts
+++ src/server/security/jwt.ts
@@ -1,8 +1,8 @@
-import Axios from 'axios';
 import { NextFunction, Request, Response } from "express";
-import { errors as JWTerrors, JWKS, JWT } from 'jose';
-import { errors as VolubleErrors } from 'voluble-common'
-    ;
+import createRemoteJWKSet from 'jose/jwks/remote';
+import jwtVerify from 'jose/jwt/verify';
+import { JWTExpired, JWTInvalid } from 'jose/util/errors';
+import { errors as VolubleErrors } from 'voluble-common';
 
 //import jwtAuthz = require('express-jwt-authz');
 
@@ -17,27 +17,23 @@ export async function checkJwt(req: Request, res: Response, next: NextFunction):
         return
     }
 
-    await Axios.get(`https://${process.env.AUTH0_VOLUBLE_TENANT}/.well-known/jwks.json`, { responseType: 'json' })
-        .then((resp) => {
-            const jwks_data = resp.data
-            const jwks = JWKS.asKeyStore(jwks_data)
-            return JWT.verify(auth_header_token, jwks, {
-                audience: process.env.AUTH0_API_ID,
-                issuer: `https://${process.env.AUTH0_VOLUBLE_TENANT}/`,
-                algorithms: ['RS256']
-            })
-        })
-        .then((_key) => {
-            Object.defineProperty(req, "user", { configurable: true, enumerable: true, writable: true, value: JWT.decode(auth_header_token) })
+    const jwks = createRemoteJWKSet(new URL("https://${process.env.AUTH0_VOLUBLE_TENANT}/.well-known/jwks.json"))
+    jwtVerify(auth_header_token, jwks, {
+        audience: process.env.AUTH0_API_ID,
+        issuer: `https://${process.env.AUTH0_VOLUBLE_TENANT}/`,
+        algorithms: ['RS256']
+    })
+        .then(result => {
+            Object.defineProperty(req, "user", { configurable: true, enumerable: true, writable: true, value: result.payload })
             return next()
         })
-        .catch((err) => {
+        .catch(err => {
 
-            if (err instanceof JWTerrors.JWTExpired) {
+            if (err instanceof JWTExpired) {
                 const serialized_err = req.app.locals.serializer.serializeError(new VolubleErrors.AuthorizationFailedError("Authorization token expired"))
                 res.status(401).json(serialized_err)
-            } else if (err instanceof JWTerrors.JWTMalformed) {
-                const serialized_err = req.app.locals.serializer.serializeError(new VolubleErrors.AuthorizationFailedError("Authorization token malformed"))
+            } else if (err instanceof JWTInvalid) {
+                const serialized_err = req.app.locals.serializer.serializeError(new VolubleErrors.AuthorizationFailedError("Authorization token invalid"))
                 res.status(401).json(serialized_err)
             } else {
                 const serialized_err = req.app.locals.serializer.serializeError(err)
